31f3d9f0011ab743677df00349936cd6d0e740a7
iver rodrigo <relativo69@gmail.com>
2025-09-22 18:27:31 -0400

    feat(pwa): enable service worker and web manifest

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideServiceWorker } from '@angular/service-worker';
+import { isDevMode } from '@angular/core';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
+  ],
+});
